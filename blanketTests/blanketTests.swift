85c5b58169dcf73bf44df202f3fdbdfc71c3de79
David Tung <david.tung@procurify.com>
2017-02-12 12:42:20 -0800

    Initial Commit

---
+++ blanketTests/blanketTests.swift
@@ -0,0 +1,36 @@
+//
+//  blanketTests.swift
+//  blanketTests
+//
+//  Created by Ta Chien Tung on 2017-02-12.
+//  Copyright © 2017 Procurify. All rights reserved.
+//
+
+import XCTest
+@testable import blanket
+
+class blanketTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
